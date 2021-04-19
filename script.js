786c0d248ae743db408a5c0a60af8e6ae6245d59
janajans <1janajansova@gmail.com>
2021-04-19 21:39:58 +0200

    splněn základ úkolu - bez bonusů

--- script.js
+++ script.js
@@ -1,45 +1,35 @@
 'use strict';
 
 //reagovat na uživatelův výběr políčka - Podle toho, kdo je na tahu, budeš přidávat do herní plochy kolečka nebo křížky a v levém horním rohu zobrazovat, čí tah následuje.
-//Vytvoř si proměnnou, ve které bude uloženo kdo je na tahu. Začíná vždy kolečko, tak rovnou do proměnné přiřaď řetězec 'circle'.
 
+// proměnná --> kdo je na tahu
 let turn = 'circle';
 
-//Pomocí posluchače událostí po kliknutí na políčko:
-// Přidej políčku příslušnou třídu podle toho, kdo je zrovna na tahu. Například board__field--circle, resp. board__field--cross.
+// Funkce move dělá:
+// - přidej políčku příslušnou třídu (kdo je na tahu)
+// - zobrazí na políčku příslušný obrázek (křížek nebo kolečko)
+// - změní v hlavičce ukazatel, kdo je na tahu
+// - střídá, kdo je na tahu
+
+const turnElm = document.querySelector('.hraje');
 
 const move = (event) => {
   if (turn === 'circle') {
     event.target.classList.add('board__field--circle');
-    event.target.innerHTML = '<img src="img/circle.svg" alt="circle move" />';
+    event.target.innerHTML = '<img src="img/circle.svg" alt="circle turn" />';
+    turnElm.innerHTML =
+      'HRAJE: <img class="turn turn--circle" src="img/circle.svg" alt="circle turn" />';
     turn = 'cross';
   } else if (turn === 'cross') {
     event.target.classList.add('board__field--cross');
-    event.target.innerHTML = '<img src="img/cross.svg" alt="cross move" />';
+    event.target.innerHTML = '<img src="img/cross.svg" alt="cross turn" />';
+    turnElm.innerHTML =
+      'HRAJE: <img class="turn turn--cross" src="img/cross.svg" alt="cross turn" />';
     turn = 'circle';
   }
 };
-
+// //Pomocí posluchače událostí po kliknutí na políčko se provede funkce move
 const buttons = document.querySelectorAll('.board__field');
 for (let i = 0; i < buttons.length; i += 1) {
   buttons[i].addEventListener('click', move);
 }
-
-// const turnElm = document.querySelector('.move');
-// moveElm.src = 'img/circle.svg';
-
-// Nastyluj políčka tak, aby nezobrazovala nic nebo kolečka a křížky podle tříd z předchozího kroku.
-
-// Změň hodnotu proměnné z 'circle' na 'cross', případně z 'cross' na 'circle'.
-
-// Uprav v levém horním rohu výpis, kdo je na tahu.
-
-// ------------SMILEY-------------:
-// const selectSmiley = (event) => {
-//   event.target.classList.add('btn-smiley--selected');
-// };
-
-// const buttons = document.querySelectorAll('.btn-smiley');
-// for (let i = 0; i < buttons.length; i += 1) {
-//   buttons[i].addEventListener('click', selectSmiley);
-// }
